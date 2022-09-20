44ae4d60ceb6d541fb99283bcffe93b9126cd1d6
Ucizi Mafeni <ucizim@gmail.com>
2022-09-20 17:29:53 +0100

    kyria: working on keys (WIP)

--- keyboards/splitkb/kyria/keymaps/yungcheeze/keymap.c
+++ keyboards/splitkb/kyria/keymaps/yungcheeze/keymap.c
@@ -27,7 +27,9 @@ enum layers {
 #define CTL_QUOT MT(MOD_RCTL, KC_QUOTE)
 #define CTL_MINS MT(MOD_RCTL, KC_MINUS)
 #define ALT_ENT  MT(MOD_LALT, KC_ENT)
-
+#define LSFT_SPC LSFT_T(KC_SPC)
+#define CTL_ENT MT(MOD_RCTL, KC_ENT)
+#define CTL_TAB MT(MOD_LCTL, KC_TAB)
 // Note: LAlt/Enter (ALT_ENT) is not the same thing as the keyboard shortcut Alt+Enter.
 // The notation `mod/tap` denotes a key that activates the modifier `mod` when held down, and
 // produces the key `tap` when tapped (i.e. pressed and released).
@@ -49,10 +51,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-     KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
-     CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     _______, KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-                                _______, KC_LGUI, ALT_ENT, KC_SPC ,_______,     _______, KC_SPC ,KC_RALT, KC_RGUI, KC_APP
+     _______  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                         KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
+     KC_GRV  , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                         KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN, KC_QUOT,
+     KC_ESC  , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , _______,_______,     _______, _______,  KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, _______,
+                                _______, KC_LWIN,LSFT_SPC, CTL_TAB,KC_LALT,     KC_LALT, CTL_ENT,LSFT_SPC, KC_LWIN, _______
     ),
 
 // /*
