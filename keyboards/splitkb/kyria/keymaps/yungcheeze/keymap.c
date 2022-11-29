a5d1c43e581928e2951ebe420b73ec4a3325d5b1
Ucizi Mafeni <ucizim@gmail.com>
2022-11-29 12:59:44 +0000

    kyria: satisfactory config

--- keyboards/splitkb/kyria/keymaps/yungcheeze/keymap.c
+++ keyboards/splitkb/kyria/keymaps/yungcheeze/keymap.c
@@ -17,21 +17,23 @@
 
 enum layers {
     _QWERTY = 0,
-    _SYM
+    _WORKMAN,
+    _NAV,
+    _NUM,
 };
 
 
 // Custom keys
 enum custom_keycodes {
-  WIN_TMUX = SAFE_RANGE,
-  EMACS_CG,
-  ALT_LPRN,
+  ALT_LPRN = SAFE_RANGE,
   CTL_RPRN,
+  LSFT_LT,
 };
 
 // Aliases for readability
 #define QWERTY   DF(_QWERTY)
-#define SYM TG(_SYM)
+#define WORKMAN   DF(_WORKMAN)
+#define NUM MO(_NUM)
 
 #define CTL_ESC  MT(MOD_LCTL, KC_ESC)
 #define CTL_QUOT MT(MOD_RCTL, KC_QUOTE)
@@ -41,20 +43,33 @@ enum custom_keycodes {
 #define LSFT_ENT MT(MOD_LSFT, KC_ENT)
 #define LSFT_TAB MT(MOD_LSFT, KC_TAB)
 #define LSFT_CAP MT(MOD_LSFT, KC_CAPS)
-#define SYM_ALT LT(_SYM, KC_LALT)
-#define SYM_L LT(_SYM, KC_L)
-#define SYM_S LT(_SYM, KC_S)
-#define WIN_TMUX MT(MOD_LGUI, LALT(KC_SEMICOLON))
+#define LSFT_S MT(MOD_LSFT, KC_S)
+#define LSFT_L MT(MOD_LSFT, KC_L)
+#define LSFT_O MT(MOD_LSFT, KC_O)
+#define NUM_CAPS LT(_NUM, KC_CAPS)
+#define NUM_ENT LT(_NUM, KC_ENT)
+#define NAV_G LT(_NAV, KC_G)
+#define NAV_V LT(_NAV, KC_V)
+#define NAV_B LT(_NAV, KC_B)
 #define TMUX LALT(KC_SEMICOLON)
 #define EMACS LALT(KC_SPACE)
 #define MA_D LALT_T(KC_D)
+#define MA_H LALT_T(KC_H)
 #define MC_F LCTL_T(KC_F)
+#define MC_T LCTL_T(KC_T)
 #define MC_J LCTL_T(KC_J)
+#define MC_N LCTL_T(KC_N)
+#define MC_6 LCTL_T(KC_6)
 #define MA_K LALT_T(KC_K)
+#define MA_E LALT_T(KC_E)
+#define MA_5 LALT_T(KC_5)
 #define SC_A LSFT_T(KC_A)
+#define SC_4 LSFT_T(KC_4)
 #define SC_SCLN LSFT_T(KC_SCLN)
+#define SC_S LSFT_T(KC_S)
 #define CTL_DOWN LCTL_T(KC_DOWN)
 #define ALT_UP LALT_T(KC_UP)
+#define WIN_CAPS MT(MOD_LGUI, KC_CAPS)
 // Note: LAlt/Enter (ALT_ENT) is not the same thing as the keyboard shortcut Alt+Enter.
 // The notation `mod/tap` denotes a key that activates the modifier `mod` when held down, and
 // produces the key `tap` when tapped (i.e. pressed and released).
@@ -76,16 +91,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-     _______ , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                         KC_Y   ,  KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
-     KC_TAB  , SC_A ,  SYM_S  ,  MA_D  ,   MC_F ,EMACS_CG,                                         KC_H   ,  MC_J ,  MA_K ,   SYM_L,SC_SCLN, KC_QUOT,
-     KC_ESC  , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , _______,_______,     _______, _______,  KC_N   ,  KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_GRV,
-                                _______,KC_LWIN ,LSFT_CAP,KC_SPC  , TMUX  ,     TMUX   ,KC_SPC  ,LSFT_ENT ,KC_LWIN, _______
+     _______ , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                         KC_Y   ,  KC_U  ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
+     KC_TAB  , KC_A ,  LSFT_S ,  MA_D  ,   MC_F , NAV_G  ,                                         KC_H   ,  MC_J  ,  MA_K ,  LSFT_L,KC_SCLN, KC_QUOT,
+     KC_ESC  , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,  KC_B  , KC_ESC ,WORKMAN,     _______, _______,  KC_N   ,  KC_M  ,KC_COMM, KC_DOT ,KC_SLSH, KC_GRV,
+                                _______,WIN_CAPS,NUM_ENT ,KC_SPC  , TMUX  ,     TMUX   ,KC_SPC  , NUM_ENT ,WIN_CAPS, _______
     ),
+
+    [_WORKMAN] = LAYOUT(
+     _______ , KC_Q ,  KC_D   ,  KC_R  ,   KC_W ,   KC_B ,                                         KC_J   ,  KC_F  ,  KC_U ,   KC_P ,KC_SCLN, KC_BSPC,
+     KC_TAB  , KC_A ,  KC_S   ,  KC_H  ,   KC_T , NAV_G  ,                                         KC_Y   ,  KC_N  ,  KC_E ,  KC_O,KC_I   , KC_QUOT,
+     KC_ESC  , KC_Z ,  KC_X   ,  KC_M  ,   KC_C ,  KC_V  , KC_ESC ,QWERTY ,     _______, _______,  KC_K   ,  KC_L  ,KC_COMM, KC_DOT ,KC_SLSH, KC_GRV,
+                                _______,WIN_CAPS,NUM_ENT ,KC_SPC  , TMUX  ,     TMUX   ,KC_SPC  , NUM_ENT ,WIN_CAPS, _______
+    ),
+
  /*
   * Layer template
   *
   * ,-------------------------------------------.                              ,-------------------------------------------.
-  * |   `    |   1  |   2  |  3   |      |      |                              |      |      |      |      |      |        |
+  * |        |      |      |      |      |      |                              |      |      |      |      |      |        |
   * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
   * |        |      |      |      |      |      |                              |      |      |      |      |      |        |
   * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
@@ -95,12 +118,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   *                        |      |      |      |      |      |  |      |      |      |      |      |
   *                        `----------------------------------'  `----------------------------------'
   */
-     [_SYM] = LAYOUT(
-       _______,   KC_1       ,   KC_2         ,   KC_3         ,   KC_4         ,   KC_5 ,                                     KC_6   ,   KC_7         ,   KC_8       ,   KC_9           ,   KC_0         , KC_EQL  ,   
-       KC_AT  , KC_LT, KC_GT, ALT_LPRN, CTL_RPRN, _______,                                     KC_LEFT, CTL_DOWN, ALT_UP, KC_RGHT, KC_PIPE, KC_MINUS,
-       KC_HASH, KC_LCBR      , KC_RCBR        , KC_LBRC        , KC_RBRC        , _______, _______, _______, _______, _______, _______, _______        , KC_CIRC      , _______          , _______        , KC_UNDS,
-                                              _______          , _______        , _______, _______, _______, _______, _______, _______, _______        , _______
+     [_NAV] = LAYOUT(
+       _______, _______, _______, _______, _______, _______,                                     _______, KC_PGDN, KC_PGUP, _______, _______, _______,
+       _______, _______, _______, _______, _______, _______,                                     KC_LEFT, CTL_DOWN, ALT_UP, KC_RGHT, _______, _______,
+       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_END, KC_HOME, _______, _______, _______,
+                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
      ),
+
+
+ /*
+  * Layer template
+  *
+  * ,-------------------------------------------.                              ,-------------------------------------------.
+  * |        |      |      |      |      |      |                              |      |      |      |      |      |        |
+  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
+  * |        |      |      |      |      |      |                              |      |      |      |      |      |        |
+  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
+  * |        |      |      |      |      |      |      |      |  |      |      |      |      |      |      |      |        |
+  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
+  *                        |      |      |      |      |      |  |      |      |      |      |      |
+  *                        |      |      |      |      |      |  |      |      |      |      |      |
+  *                        `----------------------------------'  `----------------------------------'
+  */
+     [_NUM] = LAYOUT(
+       _______, KC_HASH, KC_1   , KC_2   , KC_3   ,KC_0    ,                                     KC_PIPE, KC_LCBR, KC_RCBR, KC_CIRC, KC_HASH, _______,
+       KC_ASTR, KC_SLSH, SC_4   , MA_5   , MC_6   ,KC_EQL  ,                                    KC_MINUS,ALT_LPRN,CTL_RPRN,LSFT_LT , KC_GT  , KC_AT  ,
+       KC_PLUS,KC_MINUS, KC_7   , KC_8   , KC_9   ,KC_DOT  , _______, _______, _______, _______, KC_UNDS, KC_LBRC, KC_RBRC, _______, _______, KC_BSLS,
+                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+     ),
+
 
 // /*
 //  * Layer template
@@ -128,32 +174,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   static uint16_t my_hash_timer;
   switch (keycode) {
-    case WIN_TMUX:
-      if(record->event.pressed) {
-        my_hash_timer = timer_read();
-        register_code(KC_LWIN); // Change the key to be held here
-      } else {
-        unregister_code(KC_LWIN); // Change the key that was held here, too!
-        if (timer_elapsed(my_hash_timer) < TAPPING_TERM) {
-	  register_code(KC_LALT);
-          SEND_STRING(";"); // Change the character(s) to be sent on tap here
-	  unregister_code(KC_LALT);
-        }
-      }
-      return false; // We handled this keypress
-    case EMACS_CG:
-      if(record->event.pressed) {
-        my_hash_timer = timer_read();
-      } else {
-        if (timer_elapsed(my_hash_timer) < TAPPING_TERM) {
-	  SEND_STRING("g"); 
-        } else {
-	  register_code(KC_LCTL);
-          SEND_STRING("g"); 
-	  unregister_code(KC_LCTL);
-	}
-      }
-      return false;
     case ALT_LPRN:
       if(record->event.pressed) {
         my_hash_timer = timer_read();
@@ -164,7 +184,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           SEND_STRING("("); // Change the character(s) to be sent on tap here
         }
       }
-      return false;     
+      return false;
     case CTL_RPRN:
       if(record->event.pressed) {
         my_hash_timer = timer_read();
@@ -175,7 +195,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           SEND_STRING(")"); // Change the character(s) to be sent on tap here
         }
       }
-      return false;     
+      return false;
+    case LSFT_LT:
+      if(record->event.pressed) {
+        my_hash_timer = timer_read();
+        register_code(KC_LSFT); // Change the key to be held here
+      } else {
+        unregister_code(KC_LSFT); // Change the key that was held here, too!
+        if (timer_elapsed(my_hash_timer) < TAPPING_TERM) {
+          SEND_STRING("<"); // Change the character(s) to be sent on tap here
+        }
+      }
+      return false;
   }
   return true; // We didn't handle other keypresses
 }
@@ -194,23 +225,20 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) { return OLED_ROTATION_
 bool oled_task_user(void) {
     if (is_keyboard_master()) {
         // QMK Logo and version information
-        // clang-format off
-        static const char PROGMEM qmk_logo[] = {
-            0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-            0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-            0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
-        // clang-format on
-
-        oled_write_P(qmk_logo, false);
-
         // Host Keyboard Layer Status
         oled_write_P(PSTR("Layer: "), false);
         switch (get_highest_layer(layer_state|default_layer_state)) {
             case _QWERTY:
                 oled_write_P(PSTR("QWERTY\n"), false);
                 break;
-            case _SYM:
-                oled_write_P(PSTR("SYM\n"), false);
+            case _WORKMAN:
+                oled_write_P(PSTR("WORKMAN\n"), false);
+                break;
+            case _NAV:
+                oled_write_P(PSTR("NAV\n"), false);
+                break;
+            case _NUM:
+                oled_write_P(PSTR("NUM\n"), false);
                 break;
             default:
                 oled_write_P(PSTR("Undefined\n"), false);
@@ -222,19 +250,6 @@ bool oled_task_user(void) {
         oled_write_P(led_usb_state.caps_lock   ? PSTR("CAPLCK ") : PSTR("       "), false);
         oled_write_P(led_usb_state.scroll_lock ? PSTR("SCRLCK ") : PSTR("       "), false);
     } else {
-        // clang-format off
-        static const char PROGMEM kyria_logo[] = {
-            0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,128,128,192,224,240,112,120, 56, 60, 28, 30, 14, 14, 14,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7, 14, 14, 14, 30, 28, 60, 56,120,112,240,224,192,128,128,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-            0,  0,  0,  0,  0,  0,  0,192,224,240,124, 62, 31, 15,  7,  3,  1,128,192,224,240,120, 56, 60, 28, 30, 14, 14,  7,  7,135,231,127, 31,255,255, 31,127,231,135,  7,  7, 14, 14, 30, 28, 60, 56,120,240,224,192,128,  1,  3,  7, 15, 31, 62,124,240,224,192,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-            0,  0,  0,  0,240,252,255, 31,  7,  1,  0,  0,192,240,252,254,255,247,243,177,176, 48, 48, 48, 48, 48, 48, 48,120,254,135,  1,  0,  0,255,255,  0,  0,  1,135,254,120, 48, 48, 48, 48, 48, 48, 48,176,177,243,247,255,254,252,240,192,  0,  0,  1,  7, 31,255,252,240,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-            0,  0,  0,255,255,255,  0,  0,  0,  0,  0,254,255,255,  1,  1,  7, 30,120,225,129,131,131,134,134,140,140,152,152,177,183,254,248,224,255,255,224,248,254,183,177,152,152,140,140,134,134,131,131,129,225,120, 30,  7,  1,  1,255,255,254,  0,  0,  0,  0,  0,255,255,255,  0,  0,  0,  0,255,255,  0,  0,192,192, 48, 48,  0,  0,240,240,  0,  0,  0,  0,  0,  0,240,240,  0,  0,240,240,192,192, 48, 48, 48, 48,192,192,  0,  0, 48, 48,243,243,  0,  0,  0,  0,  0,  0, 48, 48, 48, 48, 48, 48,192,192,  0,  0,  0,  0,  0,
-            0,  0,  0,255,255,255,  0,  0,  0,  0,  0,127,255,255,128,128,224,120, 30,135,129,193,193, 97, 97, 49, 49, 25, 25,141,237,127, 31,  7,255,255,  7, 31,127,237,141, 25, 25, 49, 49, 97, 97,193,193,129,135, 30,120,224,128,128,255,255,127,  0,  0,  0,  0,  0,255,255,255,  0,  0,  0,  0, 63, 63,  3,  3, 12, 12, 48, 48,  0,  0,  0,  0, 51, 51, 51, 51, 51, 51, 15, 15,  0,  0, 63, 63,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 48, 48, 63, 63, 48, 48,  0,  0, 12, 12, 51, 51, 51, 51, 51, 51, 63, 63,  0,  0,  0,  0,  0,
-            0,  0,  0,  0, 15, 63,255,248,224,128,  0,  0,  3, 15, 63,127,255,239,207,141, 13, 12, 12, 12, 12, 12, 12, 12, 30,127,225,128,  0,  0,255,255,  0,  0,128,225,127, 30, 12, 12, 12, 12, 12, 12, 12, 13,141,207,239,255,127, 63, 15,  3,  0,  0,128,224,248,255, 63, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-            0,  0,  0,  0,  0,  0,  0,  3,  7, 15, 62,124,248,240,224,192,128,  1,  3,  7, 15, 30, 28, 60, 56,120,112,112,224,224,225,231,254,248,255,255,248,254,231,225,224,224,112,112,120, 56, 60, 28, 30, 15,  7,  3,  1,128,192,224,240,248,124, 62, 15,  7,  3,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-            0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  3,  7, 15, 14, 30, 28, 60, 56,120,112,112,112,224,224,224,224,224,224,224,224,224,224,224,224,224,224,224,224,112,112,112,120, 56, 60, 28, 30, 14, 15,  7,  3,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
-        };
-        // clang-format on
-        oled_write_raw_P(kyria_logo, sizeof(kyria_logo));
     }
     return false;
 }
@@ -246,9 +261,9 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         // Volume control
         if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
             tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
         }
     } else if (index == 1) {
         // Page up/Page down
