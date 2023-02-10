d8af5b93bd3c1422f5083f110fc78d82f005f4ed
Ucizi Mafeni <ucizim@gmail.com>
2023-02-10 16:17:07 +0000

    kyria: move caps lock to a harder to press location
    would often press it accidentally when trying to press the win key

--- keyboards/splitkb/kyria/keymaps/yungcheeze/keymap.c
+++ keyboards/splitkb/kyria/keymaps/yungcheeze/keymap.c
@@ -97,15 +97,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
      _______ , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                         KC_Y   ,  KC_U  ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
      KC_TAB  , KC_A ,  LSFT_S ,  MA_D  ,   MC_F , NAV_G  ,                                         KC_H   ,  MC_J  ,  MA_K ,  LSFT_L,KC_SCLN, KC_QUOT,
-     KC_ESC  , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,  KC_B  , KC_ESC ,WORKMAN,     _______, _______,  KC_N   ,  KC_M  ,KC_COMM, KC_DOT ,KC_SLSH, KC_GRV,
-                                KC_CAPS,KC_LGUI ,NUM_ENT ,NUM_SPC  , TMUX  ,     TMUX   ,NUM_SPC , NUM_ENT ,KC_LGUI , KC_CAPS
+     KC_ESC  , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,  KC_B  , KC_CAPS ,WORKMAN,     _______, KC_CAPS,  KC_N   ,  KC_M  ,KC_COMM, KC_DOT ,KC_SLSH, KC_GRV,
+                                _______,KC_LGUI ,NUM_ENT ,NUM_SPC  , TMUX  ,     TMUX   ,NUM_SPC , NUM_ENT ,KC_LGUI , _______
     ),
 
     [_WORKMAN] = LAYOUT(
      _______ , KC_Q ,  KC_D   ,  KC_R  ,   KC_W ,   KC_B ,                                         KC_J   ,  KC_F  ,  KC_U ,   KC_P ,KC_SCLN, KC_BSPC,
      KC_TAB  , KC_A ,  KC_S   ,  KC_H  ,   KC_T , NAV_G  ,                                         KC_Y   ,  KC_N  ,  KC_E ,  KC_O,KC_I   , KC_QUOT,
-     KC_ESC  , KC_Z ,  KC_X   ,  KC_M  ,   KC_C ,  KC_V  , KC_ESC ,QWERTY ,     _______, _______,  KC_K   ,  KC_L  ,KC_COMM, KC_DOT ,KC_SLSH, KC_GRV,
-                                KC_CAPS,KC_LGUI ,NUM_ENT ,NUM_SPC  , TMUX  ,     TMUX   ,NUM_SPC , NUM_ENT ,KC_LGUI , KC_CAPS
+     KC_ESC  , KC_Z ,  KC_X   ,  KC_M  ,   KC_C ,  KC_V  , KC_CAPS ,QWERTY ,     _______, KC_CAPS,  KC_K   ,  KC_L  ,KC_COMM, KC_DOT ,KC_SLSH, KC_GRV,
+                                _______,KC_LGUI ,NUM_ENT ,NUM_SPC  , TMUX  ,     TMUX   ,NUM_SPC , NUM_ENT ,KC_LGUI , _______
     ),
 
  /*
