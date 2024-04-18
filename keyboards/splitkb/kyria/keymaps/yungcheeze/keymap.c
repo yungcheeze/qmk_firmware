dc1c0a09050480d7d6c97b6a8f6975eadeaab6a4
Ucizi Mafeni <ucizi@turintech.ai>
2024-04-18 13:25:04 +0100

    kyria: commit to spc on left and ent on right, move tmux key closer.

--- keyboards/splitkb/kyria/keymaps/yungcheeze/keymap.c
+++ keyboards/splitkb/kyria/keymaps/yungcheeze/keymap.c
@@ -99,14 +99,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______ , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                         KC_Y   ,  KC_U  ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
      KC_TAB  , KC_A ,  LSFT_S ,  MA_D  ,   MC_F , NAV_G  ,                                         FN_H   ,  MC_J  ,  MA_K ,  LSFT_L,KC_SCLN, KC_QUOT,
      KC_ESC  , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,  KC_B  , KC_CAPS ,WORKMAN,     _______, KC_CAPS,  KC_N   ,  KC_M  ,KC_COMM, KC_DOT ,KC_SLSH, KC_GRV,
-                                _______,KC_LGUI ,NUM_ENT ,NUM_SPC  , TMUX  ,     TMUX   ,NUM_SPC , NUM_ENT ,KC_LGUI , _______
+                                _______,KC_LGUI ,NUM_SPC , TMUX    , TMUX  ,     TMUX   , TMUX   , NUM_ENT ,KC_LGUI , _______
     ),
 
     [_WORKMAN] = LAYOUT(
      _______ , KC_Q ,  KC_D   ,  KC_R  ,   KC_W ,   KC_B ,                                         KC_J   ,  KC_F  ,  KC_U ,   KC_P ,KC_SCLN, KC_BSPC,
      KC_TAB  , KC_A ,  LSFT_S   ,  MA_H  ,   MC_T , NAV_G  ,                                         FN_Y   ,  MC_N  ,  MA_E ,  LSFT_O,KC_I   , KC_QUOT,
      KC_ESC  , KC_Z ,  KC_X   ,  KC_M  ,   KC_C ,  KC_V  , KC_CAPS ,QWERTY ,     _______, KC_CAPS,  KC_K   ,  KC_L  ,KC_COMM, KC_DOT ,KC_SLSH, KC_GRV,
-                                _______,KC_LGUI ,NUM_ENT ,NUM_SPC  , TMUX  ,     TMUX   ,NUM_SPC , NUM_ENT ,KC_LGUI , _______
+                                _______,KC_LGUI ,NUM_SPC , TMUX    , TMUX  ,     TMUX   , TMUX   , NUM_ENT ,KC_LGUI , _______
     ),
 
  /*
