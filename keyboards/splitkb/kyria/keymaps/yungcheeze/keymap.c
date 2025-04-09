0ec1759e3c311f6aeaf1717662fba90540b68561
Ucizi Mafeni <ucizi@turintech.ai>
2025-04-09 09:53:32 +0100

    kyria: map Delete to unused thumb key

--- keyboards/splitkb/kyria/keymaps/yungcheeze/keymap.c
+++ keyboards/splitkb/kyria/keymaps/yungcheeze/keymap.c
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______ , KC_Q ,  KC_D   ,  KC_R  ,   KC_W ,   KC_B ,                                         KC_J   ,  KC_F  ,  KC_U ,   KC_P ,KC_SCLN, KC_BSPC,
      KC_TAB  , KC_A ,  LSFT_S   ,  MA_H  ,   MC_T , NAV_G  ,                                         FN_Y   ,  MC_N  ,  MA_E ,  LSFT_O,KC_I   , KC_QUOT,
      KC_ESC  , KC_Z ,  KC_X   ,  KC_M  ,   KC_C ,  KC_V  , KC_CAPS ,ZEN    ,     _______, KC_CAPS,  KC_K   ,  KC_L  ,KC_COMM, KC_DOT ,KC_SLSH, KC_GRV,
-                                _______,KC_LGUI ,NUM_SPC , TMUX    , TMUX  ,     FN     , TMUX   , NUM_ENT ,KC_LGUI , _______
+                                KC_DEL ,KC_LGUI ,NUM_SPC , TMUX    ,KC_ESC ,     FN     , TMUX   , NUM_ENT ,KC_LGUI , KC_DEL
     ),
 
     [_ZEN] = LAYOUT(
