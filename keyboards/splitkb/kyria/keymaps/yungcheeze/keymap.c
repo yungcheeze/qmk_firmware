24aa50c5268ec77d3802913d3db3563ceaa3b28d
Ucizi Mafeni <ucizi@turintech.ai>
2025-05-08 10:19:53 +0100

    kyria: use knob to switch windows in scrolling window managers

--- keyboards/splitkb/kyria/keymaps/yungcheeze/keymap.c
+++ keyboards/splitkb/kyria/keymaps/yungcheeze/keymap.c
@@ -250,9 +250,9 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         // Volume control
         if (clockwise) {
-            tap_code(KC_VOLD);
+            tap_code16(LGUI(KC_LEFT));
         } else {
-            tap_code(KC_VOLU);
+            tap_code16(LGUI(KC_RIGHT));
         }
     } else if (index == 1) {
         // Page up/Page down
